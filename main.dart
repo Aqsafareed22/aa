7ec7a06f6b1e166349b7426e1849ac76ef85effb
Aqsafareed22 <136243053+Aqsafareed22@users.noreply.github.com>
2023-06-30 09:20:41 -0700

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,160 @@
+//q1
+//void main() {
+  //List<int> numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//
+// for (int number in numbers) {
+  //  if (number % 2 == 0) {
+    //  print(number);
+    //}
+  //}
+//}
+//q2
+
+//void main() {
+   //Generate and print the first 10 numbers in the Fibonacci sequence
+ //for (int i = 0; i < 10; i++) {
+   // print((i));
+  //}
+//}
+
+
+
+//void main() {
+  //var num = 5;
+  //var factorial = 1;
+
+  //for( var i = num ; i >= 1; i-- ) {
+    //factorial *= i ;
+  //}
+  //print(factorial);
+
+//q5
+//void main() {
+  //int number = 6789 ;
+  //int sum = 0;
+
+  //while (number > 0) {
+    //sum += number % 10;
+    //number ~/= 10;
+  //}
+  //print('Sum of digits: $sum');
+//}
+//q6
+//void main() {
+  //List<int> numbers = [3, 9, 1, 6, 4, 2, 8, 5, 7];
+  //int largest = numbers[0];
+
+  //for (int i = 1; i < numbers.length; i++) {
+    //if (numbers[i] > largest) {
+      //largest = numbers[i];
+    //}
+  //}
+//}
+//q7
+//void main() {
+  //int number = 7;
+
+  //for (int i = 1; i <= 10; i++) {
+    //print('$number x $i = ${number * i}');
+  //}
+//}
+//q8
+//void main() {
+ // String stringToReverse = "level";
+  //String reversedString = "";
+  //String beforeReverse = stringToReverse;
+  //for (int j = stringToReverse.length - 1; j >= 0; j--) {
+    //reversedString += stringToReverse[j];
+  ///}
+  //print("Reversed String is");
+  //print(reversedString);
+  //if (beforeReverse == reversedString) {
+    //print("String is Palindrome");
+  //} else {
+   // print("String is not Palindrome");
+  //}
+///}
+//q9
+//void main() {
+  //int limit = 5;
+  //for (int i = 1; i <= limit; i++) {
+   // print('Cube of $i is ${i * i * i}');
+  //}
+//}
+//q10
+//void main() {
+  //int rows = 5;
+
+  //for (int i = 1; i <= rows; i++) {
+   // for (int j = 1; j <= i; j++) {
+     // print('*');
+    //}
+    //print('');
+  //}
+//}
+//q11
+//void main() {
+ // int rows = 5;
+
+  //for (int i = 1; i <= rows; i++) {
+   // for (int j = 1; j <= i; j++) {
+     // print(j);
+    //}
+    //print('');
+  //}
+//}
+
+
+
+//q12
+//void main() {
+ // List<int> numbers = [1, 3, 5, 7, 9, 2, 4, 6, 8, 10];
+
+  //for (int i = 0; i < numbers.length; i++) {  if (numbers[i] > 5) {
+      //print(numbers[i]);
+    //}
+  //}
+//}
+//q13
+ //void main() {
+  //String input = 'Hello, World';
+  //int vowelCount = 0;
+
+  //for (int i = 0; i < input.length; i++) {
+    //if (input[i] == 'a' || input[i] == 'e' || input[i] == 'i' || input[i] == 'o' || input[i] == 'u') {
+     // vowelCount++;
+    //} else {
+     // continue;
+    //}
+  //}
+ //}
+  //print('Number of vowels in "$input": $vowelCount');
+// }
+//q14
+  // void main() { 
+//List<int> numbers = [10, 20, 5, 2, 1];
+ // int max = numbers[1];
+  //int min = numbers[2];
+
+  //for (int i = 1; i < numbers.length; i++) {
+    //if (numbers[i] > max) {
+    //  max = numbers[i];
+  //} else if (numbers[i] < min) {
+   //   min = numbers[i];
+    //} print('Maximum element in the list: $max');
+  //print('Minimum element in the list: $min');
+//}
+ // }
+ //q15
+  //void main() {
+ //List <int>nums=[1,2,3,4,5,6,7,8,9];
+  //int sum=0;
+  //for(int num in nums){
+    //if(num%2!=0){
+     // sum +=num*num;
+    //}
+  //}
+//print('The sum of the square of all odd numbers in the list is $sum');
+//
+  //}
+ 
